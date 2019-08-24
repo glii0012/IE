8649398f83176cfba33e1fc580217ef45f3e7d77
Kaushal Vyas <kvya0001@student.monash.edu>
2019-08-25 02:14:03 +1000

    Integrated Interactive Charts

--- Dementia Care - Web/DementiaCare/App_Start/BundleConfig.cs
+++ Dementia Care - Web/DementiaCare/App_Start/BundleConfig.cs
@@ -22,6 +22,11 @@ namespace DementiaCare
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/highcharts").Include(
+                      "~/Highcharts/highcharts.js",
+                      "~/Highcharts/modules/exporting.js",
+                      "~/Highcharts/modules/export-data.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
