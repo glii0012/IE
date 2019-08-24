273ee27057731328b169d1395fad17f90f58e758
Kaushal Vyas <kvya0001@student.monash.edu>
2019-08-24 12:40:29 +1000

    .NET Project Created

---
+++ Dementia Care - Web/DementiaCare/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DementiaCare.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
