1b3c1318f068aceddf17ce1bf2a2b44d23c3c1a0
Kaushal Vyas <kvya0001@student.monash.edu>
2019-08-27 13:01:25 +1000

    Navigation Drawer Updated

---
+++ Dementia Care - App (Android)/app/src/main/java/vyas/kaushal/dementiacare/AugmentedFragment.java
@@ -0,0 +1,24 @@
+package vyas.kaushal.dementiacare;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AugmentedFragment extends Fragment {
+
+
+    public AugmentedFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_augmented, container, false);
+    }
+}
--- Dementia Care - App (Android)/app/src/main/java/vyas/kaushal/dementiacare/MainActivity.java
+++ Dementia Care - App (Android)/app/src/main/java/vyas/kaushal/dementiacare/MainActivity.java
@@ -1,44 +1,111 @@
 package vyas.kaushal.dementiacare;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import android.view.Menu;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
-    private AppBarConfiguration mAppBarConfiguration;
+    private final Fragment trainingFragment = new TrainingFragment();
+    private final Fragment puzzleFragment = new PuzzleFragment();
+    private final Fragment augmentedFragment = new AugmentedFragment();
+    private final FragmentManager fm = getSupportFragmentManager();
+    private String currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.menu_training);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_training);
+
+        fm.beginTransaction().add(R.id.nav_fragment, augmentedFragment, "augmented").hide(augmentedFragment).commit();
+        fm.beginTransaction().add(R.id.nav_fragment, puzzleFragment, "puzzle").hide(puzzleFragment).commit();
+        fm.beginTransaction().add(R.id.nav_fragment, trainingFragment, "training").commit();
+        currentFragment = "training";
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        switch (id) {
+            case R.id.nav_training:
+                if (currentFragment.equals("puzzle")) {
+                    fm.beginTransaction().hide(puzzleFragment).show(trainingFragment).commit();
+                }
+                if (currentFragment.equals("augmented")) {
+                    fm.beginTransaction().hide(augmentedFragment).show(trainingFragment).commit();
+                }
+                setTitle(R.string.menu_training);
+                navigationView.setCheckedItem(R.id.nav_training);
+                currentFragment = "training";
+                break;
+
+            case R.id.nav_puzzles:
+                if (currentFragment.equals("training")) {
+                    fm.beginTransaction().hide(trainingFragment).show(puzzleFragment).commit();
+                }
+                if (currentFragment.equals("augmented")) {
+                    fm.beginTransaction().hide(augmentedFragment).show(puzzleFragment).commit();
+                }
+                setTitle(R.string.menu_puzzle);
+                navigationView.setCheckedItem(R.id.nav_puzzles);
+                currentFragment = "puzzle";
+                break;
+
+            case R.id.nav_ar:
+                if (currentFragment.equals("training")) {
+                    fm.beginTransaction().hide(trainingFragment).show(augmentedFragment).commit();
+                }
+                if (currentFragment.equals("puzzle")) {
+                    fm.beginTransaction().hide(puzzleFragment).show(augmentedFragment).commit();
+                }
+                setTitle(R.string.menu_ar);
+                navigationView.setCheckedItem(R.id.nav_ar);
+                currentFragment = "augmented";
+                break;
+
+            default: break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
